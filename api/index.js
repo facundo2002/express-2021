043cc5cecbd6d63a586505f8b20fac531d88b91f
facundo2002 <facundovavigliano@gmail.com>
2021-11-27 11:33:17 -0300

    feat: api call libro/id

--- api/index.js
+++ api/index.js
@@ -3,17 +3,32 @@ const db = require('../models');
 
 // SELECT * FROM libro
 // Esto es una función anónima de tipo arrow function guardada en una variable llamada getBooks... por lo tanto: es una función llamada getBooks
-const getBooks = async () => {
+const getBooks = async (id) => {
     // Llamo a la DB
-    const books = await db.libro.findAll({include:db.autor}).then(result => {
+    const books = await db.libro.findAll({
+        include:db.autor}).then(result => {
         return result;
     });
 
     return books;
 }
 
-// Exportamos las funciones
-module.exports = {
-    getBooks
+const getBookById = async (id) => { 
+    console.log('+++++++++++++')
+    console.log('El Id que llego a /api es' + id)
+    console.log('+++++++++');
+   // select * from libro where id_libro = 3
+    const book = await db.libro.findByPk(id, {
+        include: db.autor
+    }).then(result => {
+        return result;
+    });
+
+    return book;
 }
 
+// Exportamos las funciones
+module.exports = {
+    getBooks,
+    getBookById
+}
--- routes/index.js
+++ routes/index.js
@@ -1,31 +1,32 @@
 var express = require('express');
 var router = express.Router();
 
-// Traigo TODAS las funciones de la API
-const api = require('../api');
+const api = require ('../api');
 
-/* GET home page. */
+/* GET home page. /
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-/* GET nosotros page */
+/ GET nosotros page /
 router.get('/nosotros', (req, res) => {
   res.render('pages/nosotros', { title: 'Nosotros' });
 });
 
-/* GET contacto page */
+/ GET contacto page */
 router.get('/contacto', (req, res) => {
   res.render('pages/contacto', { title: 'Contacto' });
 });
 
-// localhost:3000/libros
-router.get('/libros', async (req, res) => {
-  // Llamar a la función getBooks
+router.get ('/libros', async (req, res) => {
   const books = await api.getBooks();
-
-  // Devolver el JSON con los libros recibidos
   res.send(books);
+});
+
+router.get('/libro/:id', async (req,res)=>{
+ // console.log("la ruta trajo: "+ req.params.id);
+  const book = await api.getBookById(req.params.id);
+  res.send (book);
 });
 
 module.exports = router;
